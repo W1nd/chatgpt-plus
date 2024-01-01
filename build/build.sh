ac51c375c5a9dc4d8bf931811bb487f126b37779
W1nd <w1ndwkf@gmail.com>
2024-01-01 18:30:58 +0800

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -4,25 +4,29 @@ version=$1
 arch=${2:-amd64}
 
 # build go api program
-cd ../api
+cd api
 make clean $arch
 
-# build web app
-cd ../web
-npm run build
+cd ..
 
-cd ../build
+# build web app
+cd web
+npm run build
+cd ..
+
+# build docker images
+cd build
 
 # remove docker image if exists
-docker rmi -f registry.cn-shenzhen.aliyuncs.com/geekmaster/chatgpt-plus-api:$version-$arch
+docker rmi -f w1ndw/chatgpt-plus-api-go:$version-$arch
 # build docker image for chatgpt-plus-go
-docker build -t registry.cn-shenzhen.aliyuncs.com/geekmaster/chatgpt-plus-api:$version-$arch -f dockerfile-api-go ../
+docker build -t w1ndw/chatgpt-plus-api-go:$version-$arch -f dockerfile-api-go ../
 
 # build docker image for chatgpt-plus-vue
-docker rmi -f registry.cn-shenzhen.aliyuncs.com/geekmaster/chatgpt-plus-web:$version-$arch
-docker build --platform linux/amd64 -t registry.cn-shenzhen.aliyuncs.com/geekmaster/chatgpt-plus-web:$version-$arch -f dockerfile-vue ../
+docker rmi -f w1ndw/chatgpt-plus-vue:$version-$arch
+docker build --platform linux/amd64 -t w1ndw/chatgpt-plus-vue:$version-$arch -f dockerfile-vue ../
 
 if [ "$3" = "push" ];then
-  docker push registry.cn-shenzhen.aliyuncs.com/geekmaster/chatgpt-plus-api:$version-$arch
-  docker push registry.cn-shenzhen.aliyuncs.com/geekmaster/chatgpt-plus-web:$version-$arch
+  docker push w1ndw/chatgpt-plus-api-go:$version-$arch
+  docker push w1ndw/chatgpt-plus-vue:$version-$arch
 fi
